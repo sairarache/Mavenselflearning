59d07c030af992c44635ed4798429806ba64cb9a
sairarache <136586853+sairarache@users.noreply.github.com>
2024-05-09 11:52:51 +0100

    practice maven jenkins

---
+++ program/src/main/java/program1/prg2.java
@@ -0,0 +1,18 @@
+package program1;
+
+public class prg2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		
+
+ for(i=0;i<=10;i++)
+ {
+ System.out.println(i);
+ }
+ 
+	
+ }
+
+}
